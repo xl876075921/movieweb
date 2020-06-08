519134fd7eaf3180afda16ff75181d869bbac26e
xl876075921 <876075921@qq.com>
2020-06-08 19:13:19 +0800

    xule Initial commit

---
+++ user/src/main/java/com/example/user/UserApplication.java
@@ -0,0 +1,16 @@
+package com.example.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * AAAAAAAAAAAAAAAAA
+ */
+@SpringBootApplication
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+
+}
