20bf2462dde53c648fdb34eeca5dc7aa3acd7aa3
41296 <412965980@qq.com>
2020-06-09 10:18:52 +0800

    邵金辉  实验修改启动类注解77

--- user/src/main/java/com/example/user/UserApplication.java
+++ user/src/main/java/com/example/user/UserApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * AAbbb   shaojinhui1122334
+ * AAbbb   shaojinhui112233455
  * AAbbb  shaojinhui112233
  */
 @SpringBootApplication
