5c5a2d0ed08d468b1099d33148102f5c6ba7287c
John <yanjingfang@outlook.com>
2020-06-09 09:48:48 +0800

    yjf测试

--- user/src/main/java/com/example/user/UserApplication.java
+++ user/src/main/java/com/example/user/UserApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * AAAAAAAAAAAAAAAAA
+ * BBBBBBBBBBBBBBBBBBBBBBB
  */
 @SpringBootApplication
 public class UserApplication {
