881b7ecfdfab7ecc082b151e000f61e63c7400c3
41296 <412965980@qq.com>
2020-06-09 10:03:48 +0800

    邵金辉  实验修改启动类注解

--- user/src/main/java/com/example/user/UserApplication.java
+++ user/src/main/java/com/example/user/UserApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * AAbbb
+ * AAbbb   shaojinhui
  */
 @SpringBootApplication
 public class UserApplication {
