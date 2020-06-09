f7151e23839c52fafab54a2fe77a0e832ab1aee7
41296 <412965980@qq.com>
2020-06-09 15:55:57 +0800

    增加eureka模块，添加user启动类上的，添加user模块的redis配置，注册配置，mysql配置，user的pom文件添加阿里数据库

--- user/src/main/java/com/example/UserApplication.java
+++ user/src/main/java/com/example/UserApplication.java
@@ -3,12 +3,14 @@ package com.example;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * AAbbb   shaojinhui112233455
  * AAbbb  shaojinhui112233
  * hhhhhhhhhhhhh
  */
+@EnableEurekaClient
 @MapperScan("com.example.mapper")
 @SpringBootApplication
 public class UserApplication {
